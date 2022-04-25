467054d08b5181950a9b9032b08f7fb08ef1ea41
AbhishekDambalmath <abhishekmd1999@gmail.com>
2022-04-26 00:20:29 +0530

    init

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  resources :users, only: [:new, :create]
+  get 'welcome', to: 'sessions#welcome'
+  root  "sessions#welcome"
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+
+  
+  get 'authorized', to: 'sessions#page_requires_login'
+  
+end
+  
